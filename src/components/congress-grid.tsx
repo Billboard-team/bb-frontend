eb8a1b1dcac61fb8fa70a6a4db6e0fa413ec98ba
Phillip B <pbostic789@gmail.com>
2025-04-16 17:30:37 -0400

    rep card and rep grid skeleton

---
+++ src/components/congress-grid.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@chakra-ui/react";
+
+//{items} : {items : RepCardProp[]}
+export default function CongressGrid() {
+
+
+  return (
+    <Grid templateColumns="repeat(1, 1fr)" gap="3">
+
+    </Grid>
+  )
+}
---
+++ src/components/rep-card.tsx
@@ -0,0 +1,12 @@
+import { Badge, Card, Button } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"; 
+import { useState } from "react";
+import { CosponsorCardProp } from "@/components/type";
+
+export default function RepCard({item}: {item: CosponsorCardProp}) {
+
+    return (
+        <p>hi</p>
+    )
+
+}
